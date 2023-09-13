8e6458d78141c5d272d6088f42c1224d31b9ca83
Matthew Nguyen <matth3wn@gmail.com>
2023-09-13 05:52:18 -0700

    Removing firebase

--- src/components/SiteView.jsx
+++ src/components/SiteView.jsx
@@ -1,30 +1,19 @@
 import * as React from "react";
-// import { onValue, ref } from "firebase/database";
 import { Stack } from "@mui/joy";
 import { useParams } from "react-router-dom";
 import ApplicationCard from "./Card";
 import BasicModal from "./Modal";
-// import { database } from "../firebase";
+
 
 export default function SiteView() {
   const { id } = useParams();
   const [toggleModal, setToggleModal] = React.useState(false);
   const [cardData, setCardData] = React.useState({});
-  
-  // React.useEffect(() => {
-  //   return onValue(query, (snapshot) => {
-  //     const data = snapshot.val();
-  //     console.log(query)
-  //     if (snapshot.exists()) {
-  //       console.log(data)
-  //     }
-  //   });
-  // }, []);
 
-  // const handleClick = (data) => () => {
-  //   setToggleModal(true);
-  //   setCardData({...data});
-  // };
+  const handleClick = (data) => () => {
+    setToggleModal(true);
+    setCardData({...data});
+  };
   return (
     <div>
       {`Site ${id}`}
